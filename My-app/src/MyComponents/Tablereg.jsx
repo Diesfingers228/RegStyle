286c91dba1437634b77933b6a4afdcb53eabb53f
diesfingers228 <dimaarel2@gmail.com>
2023-06-03 18:41:55 +0300

    Example

--- My-app/src/MyComponents/Tablereg.jsx
+++ My-app/src/MyComponents/Tablereg.jsx
@@ -45,7 +45,7 @@ export default function Tablereg({setReg}) {
          
     };
     // styleBody
-    document.body.style.backgroundColor = 'beige';
+    document.body.style.backgroundColor = '';
 
   return (
 <main className='main'>
